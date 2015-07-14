2ce952416c400b2785ad8e5644833500007c6efb
nona1314 <syber602@gmail.com>
2015-07-14 19:42:47 +0900

    add essential operations to list

--- AjouFS.c
+++ AjouFS.c
@@ -3,9 +3,31 @@
 #include <fuse.h>
 
 
-static struct fuse_operations operations ={
-	// set operations
-	// look Function Specification in shared folder
+static struct fuse_operations operations = {
+	.init = afs_init,
+	.destroy = afs_destroy,
+
+	.getattr = afs_getattr,
+	.fgetattr = afs_fgetattr,
+
+	.create = afs_create,
+	.open = afs_open,
+	.create = afs_create,
+	.read = afs_read,
+	.wirte = afs_wirte,
+
+	.mkdir = afs_mkdir,
+	.rmdir = afs_rmdir,
+	.oepndir = afs_opendir,
+	.readir = afs_readdir,
+	.releasedir = afs_releasedir,
+	.access = afs_access,
+
+	.flush = afs_flush,
+	.release = afs_releas,
+
+	.unlink = afs_unlink,
+	.fsync = afs_fsync
 };
 
 int main(int argc, char *argv[]){
