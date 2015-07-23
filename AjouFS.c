184d17fbf76052425a2830348fa8b2735a8aa80f
minsoo713 <eldkfwkd4730@naver.com>
2015-07-23 21:17:24 +0900

    dddd

--- AjouFS.c
+++ AjouFS.c
@@ -14,7 +14,7 @@ static int afs_getattr(const char *path, struct stat * stbuf)
 
 	if(res = -1)
 	{
-		return errno;
+		return -errno;
 	}
 	return 0;
 }
@@ -83,6 +83,374 @@ static int afs_mkdir(const char *path, mode_t mode)
 	}
 	return 0;
 }
+static int afs_unlink(const char *path)
+{        
+	int res;        
+	res = unlink(path);
+
+	if (res == -1)
+	{
+		return -errno;
+	}
+	return 0;
+
+}
+static int afs_rmdir(const char *path)
+{        
+	int res;        
+	res = rmdir(path);
+        
+	if (res == -1)
+	{
+		return -errno;
+	}
+	return 0;
+}
+static int afs_symlink(const char *from, const char *to)
+{        
+	int res;        
+	res = symlink(from, to);
+        
+	if (res == -1)
+	{
+		return -errno;
+	}
+	return 0;
+}
+struct afs_dirp 
+{        
+	DIR *dp;        
+	struct dirent *entry;        
+	off_t offset;
+};
+static int afs_opendir(const char *path, struct fuse_file_info *fi)
+{        
+	int res;        
+	struct afs_dirp *d = malloc(sizeof(struct afs_dirp));
+
+	if (d == NULL)
+	{
+		return -ENOMEM;
+	}   
+	d->dp = opendir(path);
+        
+	if (d->dp == NULL) 
+	{                
+		res = -errno;               
+		free(d);                
+		return res;        
+	}
+
+	d->offset = 0;        
+	d->entry = NULL;        
+	fi->fh = (unsigned long) d;        
+	return 0;
+}
+static inline struct afs_dirp *get_dirp(struct fuse_file_info *fi)
+{        
+	return (struct afs_dirp *) (uintptr_t) fi->fh;
+}
+static int afs_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{        
+	struct afs_dirp *d = get_dirp(fi);        
+	(void) path;        
+	if (offset != d->offset) 
+	{                
+		seekdir(d->dp, offset);                
+		d->entry = NULL;                
+		d->offset = offset;        
+	}       
+	while (1) 
+	{                
+		struct stat st;                
+		off_t nextoff;                
+		if (!d->entry) 
+		{                        
+			d->entry = readdir(d->dp);
+                        
+			if (!d->entry)
+			{              
+				break;
+			}   
+		}                
+		memset(&st, 0, sizeof(st));                
+		st.st_ino = d->entry->d_ino;                
+		st.st_mode = d->entry->d_type << 12;                
+		nextoff = telldir(d->dp);                
+		if (filler(buf, d->entry->d_name, &st, nextoff))
+		{                
+			break;
+		} 
+		d->entry = NULL;                
+		d->offset = nextoff;
+        }        
+	return 0;
+}
+static int afs_releasedir(const char *path, struct fuse_file_info *fi)
+{        
+	struct afs_dirp *d = get_dirp(fi);        
+	(void) path;        
+	closedir(d->dp);        
+	free(d);        
+	return 0;
+}
+static int afs_rename(const char *from, const char *to)
+{        
+	int res;        
+	res = rename(from, to);   
+
+	if (res == -1)
+	{       
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_link(const char *from, const char *to)
+{        
+	int res;        
+	res = link(from, to);
+        
+	if (res == -1)
+	{            
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_chmod(const char *path, mode_t mode)
+{        
+	int res;        
+	res = chmod(path, mode);
+        
+	if (res == -1)
+	{
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_chown(const char *path, uid_t uid, gid_t gid)
+{        
+	int res;        
+	res = lchown(path, uid, gid);
+        
+	if (res == -1)		
+	{                
+		return -errno;		
+	}        
+	return 0;
+}
+static int afs_truncate(const char *path, off_t size)
+{        
+	int res;        
+	res = truncate(path, size);        
+	if (res == -1)
+	{           
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_ftruncate(const char *path, off_t size, struct fuse_file_info *fi)
+{        
+	int res;        
+	(void) path;        
+	res = ftruncate(fi->fh, size);
+        
+	if (res == -1)
+	{          
+		return -errno;
+	}   
+	return 0;
+}
+#ifdef HAVE_UTIMENSAT
+static int afs_utimens(const char *path, const struct timespec ts[2])
+{        
+	int res;        
+	/* don't use utime/utimes since they follow symlinks */        
+	res = utimensat(0, path, ts, AT_SYMLINK_NOFOLLOW);
+        
+	if (res == -1)
+	{          
+		return -errno;
+	}   
+	return 0;
+}
+#endif
+static int afs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{        
+	int res;        
+	(void) path;        
+	res = pread(fi->fh, buf, size, offset);
+        
+	if (res == -1)
+	{
+		res = -errno;
+	}   
+	return res;
+}
+static int afs_read_buf(const char *path, struct fuse_bufvec **bufp, size_t size, off_t offset, struct fuse_file_info *fi)
+{        
+	struct fuse_bufvec *src;        
+	(void) path;        
+	src = malloc(sizeof(struct fuse_bufvec));
+        
+	if (src == NULL)
+	{    
+		return -ENOMEM;
+	}   
+	*src = FUSE_BUFVEC_INIT(size);        
+	src->buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;        
+	src->buf[0].fd = fi->fh;        
+	src->buf[0].pos = offset;        
+	*bufp = src;        
+	return 0;
+}
+static int afs_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{        
+	int res;        
+	(void) path;        
+	res = pwrite(fi->fh, buf, size, offset);
+        
+	if (res == -1)
+	{  
+		res = -errno;
+	}   
+	return res;
+}
+static int afs_write_buf(const char *path, struct fuse_bufvec *buf, off_t offset, struct fuse_file_info *fi)
+{        
+	struct fuse_bufvec dst = FUSE_BUFVEC_INIT(fuse_buf_size(buf));        
+	(void) path;     
+	dst.buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;    
+	dst.buf[0].fd = fi->fh;    
+	dst.buf[0].pos = offset;    
+	return fuse_buf_copy(&dst, buf, FUSE_BUF_SPLICE_NONBLOCK);
+}
+static int afs_statfs(const char *path, struct statvfs *stbuf)
+{        
+	int res;        
+	res = statvfs(path, stbuf);        
+	if (res == -1)
+	{    
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_flush(const char *path, struct fuse_file_info *fi)
+{        
+	int res;        
+	(void) path;        
+	/* This is called from every close on an open file, so call the           
+    close on the underlying filesystem.  But since flush may be           
+	called multiple times for an open file, this must not really
+    close the file.  This is important if used on a network
+    filesystem like NFS which flush the data/metadata on close() */        
+	res = close(dup(fi->fh));
+
+	if (res == -1)
+	{        
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_release(const char *path, struct fuse_file_info *fi)
+{       
+	(void) path;   
+	close(fi->fh);    
+	return 0;
+}
+static int afs_fsync(const char *path, int isdatasync, struct fuse_file_info *fi)
+{        
+	int res;        
+	(void) path;
+#ifndef HAVE_FDATASYNC        
+	(void) isdatasync;
+#else        
+	if (isdatasync)
+	{   
+		res = fdatasync(fi->fh);
+	}   
+	else
+	{
+#endif                
+		res = fsync(fi->fh);
+	}		
+	if (res == -1)
+	{                
+		return -errno;
+	}        
+	return 0;
+}
+#ifdef HAVE_POSIX_FALLOCATE
+static int afs_fallocate(const char *path, int mode, off_t offset, off_t length, struct fuse_file_info *fi)
+{        
+	(void) path;
+    
+	if (mode)
+	{
+		return -EOPNOTSUPP;
+	}   
+	return -posix_fallocate(fi->fh, offset, length);
+}
+#endif
+#ifdef HAVE_SETXATTR
+/* xattr operations are optional and can safely be left unimplemented */
+static int afs_setxattr(const char *path, const char *name, const char *value, size_t size, int flags)
+{        
+	int res = lsetxattr(path, name, value, size, flags);
+        
+	if (res == -1)
+	{
+		return -errno;
+	}   
+	return 0;
+}
+static int afs_getxattr(const char *path, const char *name, char *value, size_t size)
+{       
+	int res = lgetxattr(path, name, value, size);
+        
+	if (res == -1)
+	{            
+		return -errno;
+	}   
+	return res;
+}
+static int afs_listxattr(const char *path, char *list, size_t size)
+{        
+	int res = llistxattr(path, list, size);
+        
+	if (res == -1)
+	{     
+		return -errno;
+	}   
+	return res;
+}
+static int afs_removexattr(const char *path, const char *name)
+{        
+	int res = lremovexattr(path, name);
+    
+	if (res == -1)
+	{
+		return -errno;
+	}   
+	return 0;
+}
+#endif /* HAVE_SETXATTR */
+static int afs_lock(const char *path, struct fuse_file_info *fi, int cmd, struct flock *lock)
+{        
+	(void) path;        
+	return ulockmgr_op(fi->fh, cmd, lock, &fi->lock_owner,sizeof(fi->lock_owner));
+}
+static int afs_flock(const char *path, struct fuse_file_info *fi, int op)
+{       
+	int res;        
+	(void) path;        
+	res = flock(fi->fh, op);
+        
+	if (res == -1)
+	{
+		return -errno;
+	}   
+	return 0;
+}
 void* afs_init(struct fuse_conn_info *conn){
 	return ((struct afs_pdata*)fuse_get_context()->private_data);
 }
@@ -113,9 +481,7 @@ int afs_open(const char * path, struct fuse_file_info * fi){
 	return 0;
 }
 
-int afs_read(const char * path, char * buff, size_t size, off_t offset, struct fuse_file_info * fi){
-	
-}
+
 
 static struct fuse_operations operations = {
 	.init = afs_init,
